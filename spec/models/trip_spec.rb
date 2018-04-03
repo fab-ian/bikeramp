ae1063bcf7760c83a03aac49a118c4c2882b0288
Fabian Kędzierski <grafska@gmail.com>
2018-04-03 15:11:28 +0200

    Implement another model test

--- spec/models/trip_spec.rb
+++ spec/models/trip_spec.rb
@@ -3,6 +3,8 @@
 require 'rails_helper'
 
 RSpec.describe Trip, type: :model do
+  let(:valid_data) { attributes_for(:trip) }
+
   it { should validate_presence_of(:start_address) }
   it { should validate_presence_of(:destination_address) }
   it { should validate_presence_of(:price) }
@@ -11,4 +13,9 @@ RSpec.describe Trip, type: :model do
 
   it { should validate_numericality_of(:price) }
   it { should validate_numericality_of(:distance) }
+
+  it 'can be created with valid data' do
+    trip = Trip.new(valid_data)
+    expect(trip.save).to eq(true)
+  end
 end
